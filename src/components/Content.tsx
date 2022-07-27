ac1641391d1a2fe3769a655fa4757e46247bb693
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-07-27 14:26:09 -0300

    feat: Add optimization on buttons

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,46 +4,51 @@ import { api } from "../services/api";
 import { MovieCard } from "./MovieCard";
 
 interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
+	imdbID: string;
+	Title: string;
+	Poster: string;
+	Ratings: Array<{
+		Source: string;
+		Value: string;
+	}>;
+	Runtime: string;
 }
 
 interface ContentProps {
-  selectedGenreId: number
-  selectedGenre: GenreResponseProps
+	selectedGenreId: number
+	selectedGenre: GenreResponseProps
 }
 
 export function Content({ selectedGenreId, selectedGenre }: ContentProps) {
-  // Complete aqui
 
-  const [movies, setMovies] = useState<MovieProps[]>([]);
+	const [movies, setMovies] = useState<MovieProps[]>([]);
 
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
+	useEffect(() => {
+		api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+			setMovies(response.data);
+		});
 
-  }, [selectedGenreId]);
+	}, [selectedGenreId]);
 
-  return (
-    <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
+	return (
+		<div className="container">
+			<header>
+				<span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+			</header>
 
-      <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
-      </main>
-    </div>
-  )
+			<main>
+				<div className="movies-list">
+					{movies.map(movie => (
+						<MovieCard
+							key={movie.imdbID}
+							title={movie.Title}
+							poster={movie.Poster}
+							runtime={movie.Runtime}
+							rating={movie.Ratings[0].Value}
+						/>
+					))}
+				</div>
+			</main>
+		</div>
+	)
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,36 +4,36 @@ import { api } from "../services/api";
 import { Button } from "./Button";
 
 interface SideBarProps {
-  handleClickButton: (id: number) => void
-  selectedGenreId: number
+	handleClickButton: (id: number) => void
+	selectedGenreId: number
 }
 
 export function SideBar({ handleClickButton, selectedGenreId }: SideBarProps) {
-  // Complete aqui
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+	// Complete aqui
+	const [genres, setGenres] = useState<GenreResponseProps[]>([]);
 
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
+	useEffect(() => {
+		api.get<GenreResponseProps[]>('genres').then(response => {
+			setGenres(response.data);
+		});
+	}, []);
 
-  return (
-    <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+	return (
+		<nav className="sidebar">
+			<span>Watch<p>Me</p></span>
 
-      <div className="buttons-container">
-        {genres.map(genre => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => handleClickButton(genre.id)}
-            selected={selectedGenreId === genre.id}
-          />
-        ))}
-      </div>
+			<div className="buttons-container">
+				{genres.map(genre => (
+					<Button
+						key={String(genre.id)}
+						title={genre.title}
+						iconName={genre.name}
+						onClick={() => handleClickButton(genre.id)}
+						selected={selectedGenreId === genre.id}
+					/>
+				))}
+			</div>
 
-    </nav>
-  )
+		</nav>
+	)
 }
